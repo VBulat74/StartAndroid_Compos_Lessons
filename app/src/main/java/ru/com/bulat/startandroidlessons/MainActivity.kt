1818bc85517c4bfeb75b25c72f9f791acba997f4
VBulat74 <Vladimir.Bulat@gmail.com>
2024-01-07 17:21:53 +0300

    006_State

--- app/src/main/java/ru/com/bulat/startandroidlessons/MainActivity.kt
+++ app/src/main/java/ru/com/bulat/startandroidlessons/MainActivity.kt
@@ -3,20 +3,19 @@ package ru.com.bulat.startandroidlessons
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ru.com.bulat.startandroidlessons.ui.theme.StartAndroidLessonsTheme
+import androidx.compose.runtime.mutableStateOf
+import ru.com.bulat.startandroidlessons.ui.theme.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+        val counter = mutableStateOf(0)
 
+        setContent {
+            HomeScreen(
+                count = counter,
+                {counter.value++}
+            )
         }
     }
 }
---
+++ app/src/main/java/ru/com/bulat/startandroidlessons/ui/theme/State_HomeScreen.kt
@@ -0,0 +1,19 @@
+package ru.com.bulat.startandroidlessons.ui.theme
+
+import androidx.compose.foundation.clickable
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HomeScreen(
+    count: State<Int>,
+    onCounterClick: () -> Unit
+) {
+    val counterValue = count.value
+    Text(
+        text = "Click: ${counterValue}",
+        modifier = Modifier.clickable { onCounterClick() }
+    )
+}
